4e950d3d5384272ce3761e6f0d39d2fa67d120e6
Pimegi <pimegi@gmail.com>
2024-05-22 11:25:24 +0100

    deleted old files

--- App.js
+++ App.js
@@ -1,107 +1,90 @@
-const express = require('express');
-const { connectToDatabase, getDb } = require('./db'); // Import the connectToDatabase and getDb functions
-const morgan = require('morgan');
-const path = require('path');
-const cors = require("cors"); // Cross-origin resource sharing
+//Import dependencies modules:
+const express = require('express')
+//const bodyParser = require('body-parser')
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+//Create an Express.js instance:
+const app = express()
 
-const corsOptions = {
-    origin: '*',
-    credentials: true, // access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-};
+//config Express.js
+app.use(express.json())
+app.set('port', 3000)
+app.use ((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    
+    next();
+})
 
-// Connect to MongoDB when the application starts
-connectToDatabase()
-    .then(() => {
-        const db = getDb(); // Get the database instance
+//connect to MongoDB
+const MongoClient = require('mongodb').MongoClient;
 
-        app.use(cors(corsOptions));
+let db;
+MongoClient.connect('mongodb+srv://pimegi:admin@cluster0.a07grzv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', (err, client) => {
+    db = client.db('Webstore')
+})
 
-        // Logger Middleware - Morgan
-        app.use(morgan('common'));
+//display a message for root path to show tha API is working
+app.get('/', (req, res, next) => {
+    res.send('Select a collection, e.g., /collection/messages')
+})
 
-        // Middleware to parse JSON bodies
-        app.use(express.json());
+//get the collection name
+app.param('collectionName', (req, res, next, collectionName) => {
+    req.collection = db.collection(collectionName)
+    //console.log('collection name:', req.collection)
+    return next()
+})
 
-        // Static File Middleware
-        const lessonImagesDirectory = path.join(__dirname, 'images');
-        app.use('/images', express.static(lessonImagesDirectory));
-
-        // Display a message for root path to show that API is working
-        app.get('/', (req, res, next) => {
-            res.send('Select a collection, e.g., /collection/messages');
-        });
-
-        // Get the collection name
-        app.param('collectionName', (req, res, next, collectionName) => {
-            req.collection = db.collection(collectionName);
-            return next();
-        });
-
-        // Retrieve all the objects from a collection
-        app.get('/collection/:collectionName', (req, res, next) => {
-            req.collection.find({}).toArray((e, results) => {
-                if (e) return next(e);
-                res.send(results);
-            });
-        });
-
-        // Add a new document to the collection
-        app.post('/collection/:collectionName', (req, res, next) => {
-            req.collection.insertOne(req.body, (e, result) => {
-                if (e) return next(e);
-                res.send(result.ops);
-            });
-        });
-
-        // Return a document by ID
-        const ObjectID = require('mongodb').ObjectID;
-        app.get('/collection/:collectionName/:id', (req, res, next) => {
-            req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
-                if (e) return next(e);
-                res.send(result);
-            });
-        });
-
-        // Update a document by ID
-        app.put('/collection/:collectionName/:id', (req, res, next) => {
-            req.collection.updateOne(
-                { _id: new ObjectID(req.params.id) },
-                { $set: req.body },
-                { safe: true, multi: false },
-                (e, result) => {
-                    if (e) return next(e);
-                    res.send((result.modifiedCount === 1) ? { msg: 'success' } : { msg: 'error' });
-                }
-            );
-        });
-
-        // Delete a document by ID
-        app.delete('/collection/:collectionName/:id', (req, res, next) => {
-            req.collection.deleteOne(
-                { _id: new ObjectID(req.params.id) },
-                (e, result) => {
-                    if (e) return next(e);
-                    res.send((result.deletedCount === 1) ? { msg: 'success' } : { msg: 'error' });
-                }
-            );
-        });
-
-        // Error Handling Middleware 
-        app.use((err, req, res, next) => {
-            console.error(err);
-            res.status(500).json({ message: 'Server Error' });
-        });
-
-        // Start the server
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
+//retrieve all the objects from a collection
+app.get('/collection/:collectionName', (req, res, next) => {
+    req.collection.find({}).toArray((e, results) => {
+        if (e) return next(e)
+        res.send(results)
     })
-    .catch(err => {
-        console.error('Failed to connect to MongoDB:', err);
-        process.exit(1); // Exit the process if MongoDB connection fails
-    });
+})
+
+app.listen(3000, () => {
+    console.log('Express.js server running at localhost:3000')
+})
+
+//adding post
+app.post('/collection/:collectionName', (req, res, next) => {
+    req.collection.insert(req.body, (e, results) => {
+        if (e) return next(e)
+        res.send(results.ops)
+    })
+})
+
+//return with object id
+const ObjectID = require('mongodb').ObjectID;
+app.get('/collection/:collectionName/:id'
+, (req, res, next) => {
+    req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
+        if (e) return next(e)
+        res.send(result)
+    })
+})
+
+//PUT
+app.put('/collection/:collectionName/:id', (req, res, next) => {
+    req.collection.update(
+        {_id: new ObjectID(req.params.id)},
+        {$set: req.body},
+        {safe: true, multi: false},
+        (e, result) => {
+            if (e) return next(e)
+            res.send((result.result.n === 1) ? {msg: 'success'} : {msg: 'error'})
+        })
+})
+
+//DELETE
+app.delete('/collection/:collectionName/:id', (req, res, next) => {
+    req.collection.deleteOne(
+        { _id: ObjectID(req.params.id)},
+        (e, result) => {
+            if (e) return next(e)
+            res.send((result.result.n === 1) ? {msg: 'success'} : {msg: 'error'})
+        })
+})
